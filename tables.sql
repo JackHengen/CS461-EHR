2a3367af61328ebe300e8af8f69da14f2e826ea1
Wliss0885 <wliss0885@gmail.com>
2026-05-02 23:33:47 -0400

    Creation of doctor and medication tables, also added doctor id to patient as a foreign key

--- tables.sql
+++ tables.sql
@@ -11,7 +11,9 @@ CREATE TABLE Patient(
     gender VARCHAR(25),
     pronouns VARCHAR(10),
     dob DATE,
+    did INT,
     PRIMARY KEY (pid)
+    FOREIGN KEY (did) REFERENCES Doctor(did)
 );
 
 CREATE TABLE PatientMeasurement(
@@ -35,3 +37,19 @@ CREATE TABLE PatientPhone(
     phone_num VARCHAR(50),
     FOREIGN KEY (pid) REFERENCES Patient(pid)
 );
+
+CREATE TABLE Doctor(
+    did INT,
+    specialty VARCHAR(255),
+    fname VARCHAR(255),
+    lname VARCHAR(255)
+)
+
+CREATE TABLE Medication(
+    mid INT,
+    name VARCHAR(255),
+    dosage INT,
+    description VARCHAR(255)
+    pid INT,
+    FOREIGN KEY (pid) REFERENCES Patient(pid)
+)
